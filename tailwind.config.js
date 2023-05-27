74b3b47e6a312e3fae51285e4ff346f6c7444a8d
Lance Armah-Abraham <lance@vdc.cloud>
2023-05-27 16:13:58 +0000

    feat: update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,25 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
-		extend: {}
+		extend: {
+			fontFamily: {
+				'mystery-quest': ['Mystery Quest', ...defaultTheme.fontFamily.sans],
+				sans: ['Montserrat', ...defaultTheme.fontFamily.sans]
+			},
+			colors: {
+				primary: {
+					100: '#E0E0E0',
+					200: '#291507'
+				},
+				secondary: {
+					100: '#E3E1DC',
+					200: '#544439'
+				}
+			}
+		}
 	},
 	plugins: []
 };
